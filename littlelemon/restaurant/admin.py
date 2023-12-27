10cb2455e17bba9d9c08dd3a3f590fbfed59028f
Sebastian Ferreyra <sebastianferreyrafic@gmail.com>
2023-12-27 11:20:47 -0300

    added models

--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Booking, Table
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'no_of_guests', 'booking_date')
+    search_fields = ('name',)
+    date_hierarchy = 'booking_date'
+
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'price', 'inventory')
+    search_fields = ('title',)
+
+# Register models with the custom admin classes
+admin.site.register(Booking, BookingAdmin)
+admin.site.register(Table, TableAdmin)
